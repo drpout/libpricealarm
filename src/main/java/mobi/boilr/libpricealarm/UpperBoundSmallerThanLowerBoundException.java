08ac98f04476855ebfdcf71556fac05e654edeb4
dllud <david.ludovino@gmail.com>
2014-09-25 15:19:08 +0100

    Added message to UpperBoundSmallerThanLowerBoundException.

--- src/main/java/mobi/boilr/libpricealarm/UpperBoundSmallerThanLowerBoundException.java
+++ src/main/java/mobi/boilr/libpricealarm/UpperBoundSmallerThanLowerBoundException.java
@@ -4,4 +4,8 @@ public class UpperBoundSmallerThanLowerBoundException extends Exception {
 
 	private static final long serialVersionUID = 3260364362230403962L;
 
+	public UpperBoundSmallerThanLowerBoundException() {
+		super("Upper bound is smaller than lower bound.");
+	}
+
 }
